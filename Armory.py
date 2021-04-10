d192265750c5935f7e50fa33ac3cf43de956f7ec
leoredko <leoredko@gmail.com>
2021-04-10 19:32:44 -0400

    Class Enemies in the armory

---
+++ Armory.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pygame as pyg
+
+
+class Zombie:
+    	def __init__(self):
+		self.hp = 6
+		self.dmg = 2
+
+class Dead_Team_Member:
+	def __init__(self):
+		self.hp = 15
+		self.dmg = 5
+
+class Wild_Dog:
+	def __init__(self):
+		self.hp = 3
+		self.dmg = 1
