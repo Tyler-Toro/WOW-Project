4d8810794a0bc6a25900d9a9165a56f3a0ac00fa
Tyler Toro <tyler.toro1@gmail.com>
2021-04-13 20:56:57 -0400

    Updated intro file w/ user class

--- ETS_intro.py
+++ ETS_intro.py
@@ -1,7 +1,7 @@
 from ETS_functions import *
 
 
-def introduction(user_name, user_city, user_country):
+def introduction(ETS_user):
     storyline_paragraph("\n\nA single flash of lightning casts dozens of long shadows across the skybridge. The subsequent\n\
 thunder crashes, deafening the small group. The rain is relentless, making the view in every direction a grey abyss.\n\
 The only feature made out in the distance, as another flash of lightening blazes across the sky, is the outline \n\
@@ -9,18 +9,18 @@ of a gigantic building. An entire team has been missing from the rendezvous for 
 Someone is shouting over the rain, gripping a radio...\n\n")
     
     pause()
-    slow_text(f"\n\t({user_name}): \"....-repeat, Systems check! Over!\"\n")
+    slow_text(f"\n\t({ETS_user.user_name}): \"....-repeat, Systems check! Over!\"\n")
     slow_text("\n......\n")
-    slow_text(f"\n\t({user_name}): \"...Systems check, come in Team Bravo!\"\n")
+    slow_text(f"\n\t({ETS_user.user_name}): \"...Systems check, come in Team Bravo!\"\n")
     slow_text("\n...(radio silence)...\n")
     slow_text("\n....<<<static>>>\n")
-    slow_text(f"\n\t({user_name}): \"BRAVO, DO YOU READ???!!!\"\n")
+    slow_text(f"\n\t({ETS_user.user_name}): \"BRAVO, DO YOU READ???!!!\"\n")
     slow_text("\n...(radio silence)...\n")
-    slow_text(f"\n\t({user_name}): \"Damn!! Alpha, move out!\"\n\n")
+    slow_text(f"\n\t({ETS_user.user_name}): \"Damn!! Alpha, move out!\"\n\n")
     time.sleep(slow)
     storyline_paragraph(f"\nRain continues to pour in sheets as Alpha team scans the lobby of Toro Sanctum, a newly constructed \n\
-bioengineering facility on {user_city.title()} Island. After the \"Incindent\", the paramilitary contractors of PiThon were \n\
-flown in from {user_country.title()} to inspect Toro Sanctum and the surrounding coast. Team Bravo led the expedition \n\
+bioengineering facility on {ETS_user.user_city.title()} Island. After the \"Incindent\", the paramilitary contractors of PiThon were \n\
+flown in from {ETS_user.user_country.title()} to inspect Toro Sanctum and the surrounding coast. Team Bravo led the expedition \n\
 into the facility while Team Alpha scanned the ports of entry and docks. There was nothing. No signs of anyone \n\
 having visited or worked in the high tech research facility which celebrated its launch two weeks ago. No ships. \n\
 No vehicles on the newly paved parking lots. Nothing. Team Alpha lost communication with Bravo squad just as the \n\
