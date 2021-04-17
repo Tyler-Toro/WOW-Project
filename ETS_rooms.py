3d187213d16c48f29ecc04c228405d3a2534b018
Tyler Toro <tyler.toro1@gmail.com>
2021-04-16 22:32:16 -0400

    Added rooftop storyline

--- ETS_rooms.py
+++ ETS_rooms.py
@@ -16,9 +16,11 @@ def escaping_sanctum(ETS_user):
 
 
 def main_lobby(ETS_user):
+    slow_text("\n\t\t\t __Main Lobby__\t\n\n")
     storyline_paragraph(f"\nAlpha team continues onward, moving past the lobby. The polished linoleum floors reflect each movement. \n\
 The dim emergency lights flicker, drawing the attention of the team. The rain becomes more faint as they reach the elevator and \n\
-staircase section at the end of the empty lobby. There is a sudden muffled sound that freezes {ETS_user.user_name} in their tracks\n")
+staircase section at the end of the lobby. The empty elevator shafts are hollowed silos extending in each direction. There is \n\
+a sudden muffled sound that freezes {ETS_user.user_name} in their tracks\n")
     pause()
     slow_text("\n\n...<<distant radio static>>..\n")
     slow_text(f"\n\t({ETS_user.user_name}): \"Team Alpha! Hold, I hear something..\"\n")
@@ -42,7 +44,7 @@ staircase section at the end of the empty lobby. There is a sudden muffled sound
         time.sleep(quick)
         roof(ETS_user)
     elif escaping_sanctum2 == "4":
-        storyline_paragraph(f"\n{ETS_user.user_name} leads the Team towards the center elevator and peers into the slightly opened doors")
+        storyline_paragraph(f"\n{ETS_user.user_name} leads the Team towards the center elevator and peers into the darkness below")
         slow_text(f"\n\t({ETS_user.user_name}): \"Thats a long way down...hook me up to that safety latch. I'm going down. Cover me\"\n")
         storyline_paragraph(f"\nAfter securing the tactical rope, {ETS_user.user_name} slowly makes their decent..\n\
 Once inside the poorly lit elevator shaft, a small sign comes into view..\n")
@@ -68,7 +70,8 @@ Once inside the poorly lit elevator shaft, a small sign comes into view..\n")
 ##############################################################################
 
 def basement(ETS_user):
-    storyline_paragraph(f"\n\nAlpha Team heads toward the frosted glass doors that lead to the stairs, the basement a dozen flights down. \n\
+    slow_text("\n\t\t\t __Basment__ \t\n\n")
+    storyline_paragraph(f"\n\nAlpha Team heads toward the frosted glass doors that lead to the stairs, the basement many flights down. \n\
 With a slight push of the doors, radio static can be fainlty heard emanating from below. {ETS_user.user_color.title()} lights illuminate ascending \n\
 and decending steps, but the team heads towards the noise..\n")
     pause()
@@ -106,6 +109,7 @@ There is a directory on the wall\n")
 
 
 def basement_unitA(ETS_user):
+    slow_text("\n\t\t\t __Basement: Unit A__\t\n\n")
     storyline_paragraph(f"\nTeam Alpha continues slowly down the corridor towards the storage bay. The staircase lights become less and less\n\
 useful with each step as they head further into darkness. The only thing visible in the distance is a small window hovering on the door of \n\
 Storage Unit A. The dim {ETS_user.user_color} light creeping through is barely visible, but the shadow of a figure flashes past..\n\n")
@@ -129,6 +133,7 @@ a deep bite wound. The outline of Toro Sanctum is reflected off their gaze as th
 ###############################################################################################
 
 def basement_unitB(ETS_user):
+    slow_text("\n\t\t\t __Basement: Unit B__\t\n\n")
     storyline_paragraph(f"\nTeam Alpha makes a right and heads towards the frozen storage. The flickering {ETS_user.user_color} lights overhead \n\
 crack and blink as they approach the freezer door. Through the icy window, there is a blinking LED light glowing on the ground in \n\
 the darkness. The LED light is the same as on everyone\'s radio.\n")
@@ -159,26 +164,24 @@ The slowing flash LED light tints the faces engulfing {ETS_user.user_name}...\n"
 ####################################################################
 
 def lab(ETS_user):
-    storyline_paragraph("\n\nAlpha Team begins to make their way out of the area when a sudden rumble is felt...\n")
-    fast_text("\n_._.._...............::::::::::::;;;;;;;;;;;;;;;;;;;;######################<CRASH>!!!!!!!!!\n\n")
-    storyline_paragraph("\nAll at once an elevator car comes screeching past the center shaft, coming to a crash many floors below")
-    slow_text(f"\n\t({ETS_user.user_name}): \"WHAT IS GOING ON HERE?!\"\n")
-    storyline_paragraph(f"\n{ETS_user.user_name.title()} peers over the ledge, then regroups the team and heads down a large corridor.\n\
+    slow_text("\n\t\t\t __Laboratory__\t\n\n")
+    storyline_paragraph("\n\nAlpha Team begins to make their way towards the sealed glass doors of the laboratory wing.\n\
+The powerless automatic doors are pushed open with ease as Team Alpha enters the decontamination area. There is a sudden rumble felt...\n\
+From many levels below comes the sound of heavy machinery and gears slowly grinding to a halt.")
+    slow_text(f"\n\t({ETS_user.user_name}): \"What is going on here? The elevators were dead..\"\n")
+    storyline_paragraph(f"\n{ETS_user.user_name} peers back towards the lobby, then regroups the team and heads down a large corridor.\n\
 Onward, Alpha Team can see flashing {ETS_user.user_color.title()} lights through the large glass doors of the lab.\n")
     pause()
-    storyline_paragraph(f"\nThe laboratory doors slide against a gritty floor. The lab is field of broken glass and smoke.\n\
-{user_color.title()} light is scattered in every direction as the team moves toward the emergency exit. The emergency exit \n\
+    storyline_paragraph(f"\nThe laboratory doors slide against a gritty floor. The lab is field of broken glass and debris.\n\
+{ETS_user.user_color.title()} light is scattered in every direction as the team moves toward the emergency exit. The emergency exit \n\
 doors are blocked by a mountain of large boxes, lab equipment, and garbage\n")
     pause()
     if ETS_user.user_item == "A lucky coin" or ETS_user.user_item == "A golden locket" or ETS_user.user_item == "A bronze compass":
-        slow_text(f"{ETS_user.user_item} falls out of {ETS_user.user_name}\'s pocket and rolls towards the lab entrance, making it to the corridor\n")
+        slow_text(f"{ETS_user.user_item} falls out of {ETS_user.user_name}\'s pocket and rolls out of the lab entrance, making it to the corridor\n")
         time.sleep(slow)
         slow_text(f"\n\t({ETS_user.user_name}): \"...strange..\"\n\n")
     else:
         pass
-
-    ##### Both choices fork off in Lab #########################################
-    ############################################################################
 
     lab_fork = input(f"{ETS_user.user_name}, what will you do next? \n\
     [Select 1 or 2, then press <enter> ]\n\n\
@@ -200,12 +203,13 @@ doors are blocked by a mountain of large boxes, lab equipment, and garbage\n")
 #####################################################################
 
 def lab_onward(ETS_user):
+    slow_text("\n\t\t\t Laboratory: Emergency Exit \t\n\n")
     storyline_paragraph(f"\nThe Team begins unblocking the emergency exit doors. The massive equipment pieces take the \n\
 entire team to move aside. Shards of glass fall off of repositioned boxes. Something catches {ETS_user.user_name}\'s eye.\n")
     pause()
     storyline_paragraph("\nAmong the glass are pieces of shredded clothing and hair\n\n")
     slow_text(".............................\n")
-    slow_text(f"\n\t({ETS_user.user_name}): \"...WAIT....ALPHA TEAM FALL BAC------\n")
+    slow_text(f"\n\t({ETS_user.user_name}): \"......WAIT....ALPHA TEAM FALL BAC------\n")
     storyline_paragraph("\nThe exit doors plunge open. Alpha team is bombarded, surrounded in seconds\n")
     header()
     retry = input("Game Over. Try again? (Y/N)\n")
@@ -224,38 +228,46 @@ entire team to move aside. Shards of glass fall off of repositioned boxes. Somet
 ############################################################################################
 
 def lab_flee(ETS_user):
-    print(f"\n{ETS_user.user_name} guides Alpha Team back to the lobby and to the entrance of the main stairway\n")
+    storyline_paragraph(f"\n{ETS_user.user_name} guides Alpha Team back to the lobby and to the entrance of the main stairway\n")
     basement(ETS_user)
 
 
+### Roof Choice ##########################################################
+##########################################################################
+
 def roof(ETS_user):
+    slow_text("\n\t\t\t __Rooftop__\t\n\n")
     storyline_paragraph(f"\n\nTeam Alpha led by {ETS_user.user_name} heads towards the staircase and starts the long climb to the roof\n\
 access door. The light becomes dimmer and dimmer as they accend, the faint outline of a large metal door seems further away with each step.\n\
 The sound of rain and distant thunber can be felt humming behind the steel door. The door is being held open by something... \n\
 {ETS_user.user_name} reaches down and pushes the door slightly to grab the small object")
     slow_text(f"\n\t({ETS_user.user_name}): \"What is this? It looks like a small bone...\n")
-    storyline_paragraph("\nThe Team kicks open the door and rushes the rooftop. In every direction are bullet casings and empty ammunition\n\
-crates. The helicopter on the helipad is destroyed; a twisted metal skeleton with it\'s propellers folded. The team moves further onto the roof when\n\
-the metal door behind them slams shut")
+    storyline_paragraph(f"\nThe Team kicks open the door and rushes the rooftop. In every direction are bullet casings and empty ammunition\n\
+crates. The helicopter on the helipad is destroyed; a twisted metal skeleton with it\'s propellers folded. The AH-{(str(roof_access[0]))+(str(roof_access[1]))}\n\
+Apache helicopter is military grade...The team moves further onto the roof when the metal door behind them slams shut")
     pause()
     storyline_paragraph("The door is reinforced steel with a blinking keypad above the door handle.\n")
-    slow_text("\t_ _ _ _\n")
+    slow_text("\t[ _ _ _ _ ]\n")
     slow_text(f"\n\t({ETS_user.user_name}): \"Dammit! The keypad barely has power supply.\n")
     storyline_paragraph(f"\nStill gripping the small bone, {ETS_user.user_name} feels something carved into it. There are a series of numbers but some are\n\
-too difficult to make out. All that is legible is _ _ {roof_access[2:]}")
+too difficult to make out. All that is legible is \"_ _ {roof_access[2:]}\"")
     print(roof_access)
+    #print((str(roof_access[0]))+(str(roof_access[1])))
     attempt_count = 0
     while attempt_count < 3:                                      # PyTest ??
         roof_attempt = input("\n\t Enter the correct access pin:")
         if roof_attempt == roof_access:
-            print("Access granted")
-            main_lobby(ETS_user)
+            slow_text("\n\t<<<Access granted>>>\n\n")
+            pause()
+            storyline_paragraph(f"The team rushes through the door and races down the steps in growing darkness. {ETS_user.user_name} shuts the heavy\n\
+door just as decayed arms and hands are within reach. The team heads straight to the basement.\n")
+            basement(ETS_user)
             break
         else:
             attempt_count += 1   # PlUS EQUALS 5 HEAD
-            print("incorrect") 
+            slow_text("\n\tinvalid entry\n") 
     if attempt_count == 3:
-        storyline_paragraph(f"An alarm blares from the rooftop and echoes throughout the island. The access door keypad flashes {ETS_user.user_color}\n\
+        storyline_paragraph(f"\n\nAn alarm blares from the rooftop and echoes throughout the island. The access door keypad flashes {ETS_user.user_color}\n\
 as smoke rises from the power circuit. The alarm continues on as flocks of birds scatter off of distant tree tops. As the sirens screech...a closer\n\
 sound becomes audible...")
         pause()
@@ -269,8 +281,25 @@ orders the team to rappel down to the nearest window")
         
 
 def roof_escape(ETS_user):
-    storyline_paragraph("The team smashes the window and lunges inside")
-    lab(ETS_user)
+    storyline_paragraph(f"\nThe sound of harnesses zipping down the rappel lines buzz like a thousand angry wasps. The team soars down and lunges inside\n\
+the nearest row of windows several landings below. Crashing through glass, {ETS_user.user_name} disconnects the rappel line and lands on a \n\
+concrete surface. The room is dark and humid, the air acrid.\n")
+    pause()
+    slow_text("....................\n")
+    slow_text("\n....................\n")
+    slow_text(f"\n\t({ETS_user.user_name}): Something's not right in here...\"\n")
+    fast_text("\n...........hhhHHHHRNNNGAAA!!!\n")
+    storyline_paragraph(f"\n{ETS_user.user_name} raises a flashlight and illuminates the faces of a zombie hoard rushing toward Team Alpha..\n")
+    retry = input("Game Over. Try again? (Y/N)\n")
+    if retry == "n" or retry == "N":
+        sys.exit()
+    elif retry == "y" or retry == "Y":
+        time.sleep(step)
+        escaping_sanctum(ETS_user)
+    else:
+        time.sleep(step)
+        print("\n\t\tInvalid response".upper())
+        roof_escape(ETS_user)
 
 
 ## Puzzle variables ########################
