221831f3c70a7057968d5c4f7b25dccd976fd665
Leo Redko <78003415+leoredko@users.noreply.github.com>
2021-05-10 15:56:03 -0400

    added another enemy class

--- ETS_battles.py
+++ ETS_battles.py
@@ -23,3 +23,27 @@ def zombie_battle(ETS_user):
        sys.exit()
     else: 
         print('You have defeated the zombie!')
+        
+class Dead_team_member:
+    def __init__(self):
+        self.hp = 15
+        self.dmg = 5
+    def update_health(self,hp_change):
+        self.hp = self.hp + hp_change
+
+def Dead_team_member_battle(ETS_user):
+    dead_team_member = Dead_team_member()
+    while dead_team_member.hp > 0 and ETS_user.hp > 0:
+        user_input = input('Press A and then Enter to attack the zombie!' /n).lower()
+        if user_input == 'a': 
+            dead_team_member.update_health(ETS_user.dmg * -1)
+            print(f"You have shot and dealt {ETS_user.dmg} damgage to the dead team member")
+        ETS_user.update_health(zombie.dmg * -1)
+        print(f"You have been dealt {dead_team_member.dmg} damage by the dead team member")
+        
+    
+    if ETS_user.hp <= 0:
+       print('You were killed by a dead team member, Game Over!')
+       sys.exit()
+    else: 
+        print('You have defeated the dead team member!')
